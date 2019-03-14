e6f5336d5b2a5ffb2d81e00ef8d14cfafd3748b9
sebastian <sebastian.sledz@gmail.com>
2019-03-15 00:17:11 +0100

    hero update

--- main.js
+++ main.js
@@ -23,3 +23,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   
   });
+
+var rn = Math.floor((Math.random() * 150) + 60);
+var rs = Math.floor((Math.random() * 11) + 4);
+	var t = new Trianglify({
+ x_gradient: Trianglify.colorbrewer.Spectral[rs],
+    noiseIntensity: 0,
+    cellsize: rn
+});
+var pattern = t.generate(window.innerWidth, window.innerWidth+200);
+document.getElementById("hero").setAttribute('style', 'background-image: '+pattern.dataUrl);

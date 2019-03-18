98e68ab8cd6a29ccc10775b0ac0203f47ab40d26
sebastian <sebastian.sledz@gmail.com>
2019-03-18 21:36:08 +0100

    hero-bg-refresh-update

--- main.js
+++ main.js
@@ -24,12 +24,18 @@ document.addEventListener('DOMContentLoaded', () => {
   
   });
 
-var rn = Math.floor((Math.random() * 150) + 60);
-var rs = Math.floor((Math.random() * 11) + 4);
-	var t = new Trianglify({
- x_gradient: Trianglify.colorbrewer.Spectral[rs],
-    noiseIntensity: 0,
-    cellsize: rn
-});
-var pattern = t.generate(window.innerWidth, window.innerWidth+200);
-document.getElementById("hero").setAttribute('style', 'background-image: '+pattern.dataUrl);
+  window.onload = function changeBG() {
+
+      var rn = Math.floor((Math.random() * 150) + 60);
+      var rs = Math.floor((Math.random() * 11) + 4);
+        var t = new Trianglify({
+       x_gradient: Trianglify.colorbrewer.Spectral[rs],
+          noiseIntensity: 0,
+          cellsize: rn
+      });
+      var pattern = t.generate(window.innerWidth, window.innerWidth+200);
+      document.getElementById("hero").setAttribute('style', 'background-image: '+pattern.dataUrl);
+      document.getElementById("refresh-bg").addEventListener("click", changeBG);
+
+
+}
